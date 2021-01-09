82eec2d2b4368535b45b04b96b088b8df8a4dd17
mursalinEmon <mursalinemon@gmail.com>
2021-01-09 12:36:26 +0600

    dashboard bug solved

--- back-end/app/Http/Controllers/StudentProfileController.php
+++ back-end/app/Http/Controllers/StudentProfileController.php
@@ -15,9 +15,13 @@ class StudentProfileController extends Controller
      */
     public function index()
     {
-        $student = StudentProfile::findOrFail(auth()->user()->id);
+
+        $student = StudentProfile::where('user_id',auth()->user()->id)->first();
+
         $courses=$student->enrolled_courses;
+
         $enrolled_course_count=count($courses);
+
         return view('student.student_dashboard',compact('enrolled_course_count'));
     }
 
