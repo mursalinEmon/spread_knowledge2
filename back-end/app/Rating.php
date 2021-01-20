1fb041342f34542ff6fd98adb1a5859b8ddd3f91
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-01-20 16:34:57 +0600

    relation set up

--- back-end/app/Rating.php
+++ back-end/app/Rating.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use App\Course;
+use App\StudentProfile;
 use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
-    //
+    protected $guarded=[];
+
+    public function student(){
+        return $this->belongsTo(StudentProfile::class,'student_id','id');
+    }
+    public function course(){
+        return $this->belongsTo(Course::class,'course_id','id');
+    }
 }
--- back-end/app/StudentProfile.php
+++ back-end/app/StudentProfile.php
@@ -11,5 +11,8 @@ class StudentProfile extends Model
     protected $casts = [
         'enrolled_courses' => 'array',
     ];
+    public function ratings(){
+        return $this->hasMany(Rating::class);
+    }
 
 }
